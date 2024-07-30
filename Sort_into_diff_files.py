602d78a90a4965df844a1f5ae9f5592d11805803
Kyung-Wan Woo <dnruddhks1223@gmail.com>
2024-07-30 13:07:39 +0900

    updated with countries and emails

--- Sort_into_diff_files.py
+++ Sort_into_diff_files.py
@@ -3,29 +3,38 @@ import re
 import os
 
 # Load the provided Excel file
-file_path = '/Users/kyungwanwoo/Downloads/contact_export_1102701457545_070224_080255.xls'
+file_path = '/Users/kyungwanwoo/Downloads/contact_export_1102701457545_072924_112559.xls'
 df = pd.read_excel(file_path, engine='xlrd')
 
 # Define categories and corresponding keywords
 categories = {
-    "University": ["university", "college", "institute", "school", "academy", "u ", "u. ", "univ", "polytechnic", "community college", "universite", "edu", "ucla", "ucsf"],
+    "University": ["university", "college", "institute", "school", "academy", "u ", "u. ", "univ", "polytechnic", "community college", "universite", "edu", "UCLA", "UCSF", "UNSW"],
     "NGO": ["council", "foundation", "ngo", "non-profit", "nonprofit", "association", "charity", "federation", "society", "network", "cooperative"],
-    "Government": ["department", "ministry", "agency", "government", "bureau", "office", "commission", "administration", "authority", "board", "service", "gov", "mil"],
-    "Private": ["inc", "corp", "company", "llc", "limited", "co.", "group", "industries", "enterprise", "ventures", "solutions", "technologies", "systems"],
+    "Government": ["department", "ministry", "agency", "government", "bureau", "office", "commission", "administration", "authority", "board", "service", "gov", "mil", "NIH", "CDC", "WHRI", "USAID"],
+    "Private": ["inc", "corp", "company", "llc", "limited", "co.", "group", "industries", "enterprise", "ventures", "solutions", "technologies", "systems", "Intel", "IBM Watson Health", ],
     "Healthcare": ["hospital", "clinic", "health center", "medical center", "health services", "healthcare", "health foundation"],
-    "Research": ["research institute", "research center", "laboratory", "research foundation", "biomedical"],
+    "Research": ["research institute", "research center", "laboratory", "research foundation", "biomedical", "Wits RHI"],
     "Non-Profit": ["non-profit", "nonprofit", "charity", "foundation", "volunteer", "service organization"],
     "Educational": ["primary school", "secondary school", "high school", "elementary school", "middle school", "kindergarten", "academy"],
     "Industry Association": ["association", "federation", "chamber of commerce", "society", "consortium", "union"],
-    "International Organization": ["united nations", "world bank", "international monetary fund", "world health organization", "UNICEF", "UNESCO", "international"],
+    "International Organization": ["united nations", "world bank", "international monetary fund", "world health organization", "WHO","UNICEF", "UNESCO", "UNFPA", "UNITAID", "Unitaid", "international", "int"],
+    "UK": ["London", "UK"],
+    "India": ["India"],
+    "Canada": ["Canada"],
+    "Australia": ["Australia"],
     "Other": []  # Any other keywords if needed
 }
 
 # Add domain-based keywords to categories
 domain_categories = {
-    "University": ["edu"],
-    "Government": ["gov", "mil"],
-    "Non-Profit": ["org"]
+    "University": ["edu", "ac"],
+    "Government": ["gov", "mil", "state", "govt", "us", "ca"],
+    "Non-Profit": ["org", "ngo"],
+    "International Organization": ["int"],
+    "UK": ["uk"],
+    "India": ["in"],
+    "Canada": ["ca"],
+    "Australia": ["au"],
 }
 
 # Function to check domain-based categorization
@@ -43,12 +52,12 @@ os.makedirs(output_dir, exist_ok=True)
 # Filter and save each category to a different Excel file
 for category, keywords in categories.items():
     if keywords:  # Only filter if there are keywords defined
-        category_df = df[df['Company'].str.contains('|'.join(keywords), case=False, na=False) | df['Email'].apply(lambda x: categorize_by_domain(x) == category if pd.notna(x) else False)]
+        category_df = df[df['Company'].str.contains('|'.join(keywords), case=False, na=False) | df['Email address'].apply(lambda x: categorize_by_domain(x) == category if pd.notna(x) else False)]
     else:  # Handle the "other" category
         known_keywords = [item for sublist in categories.values() for item in sublist]
         category_df = df[~df['Company'].str.contains('|'.join(known_keywords), case=False, na=False)]
         known_domains = [item for sublist in domain_categories.values() for item in sublist]
-        category_df = category_df[~category_df['Email'].apply(lambda x: categorize_by_domain(x) in domain_categories if pd.notna(x) else False)]
+        category_df = category_df[~category_df['Email address'].apply(lambda x: categorize_by_domain(x) in domain_categories if pd.notna(x) else False)]
 
     # Define the output path for the category
     output_path = os.path.join(output_dir, f'{category}_contacts.xlsx')
